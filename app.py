642b878cf3fe1c94c88a0fbce61346354359f549
eecs4413 friends <sunglassbungalow@gmail.com>
2024-05-07 14:05:39 -0400

    Modified Code and tests for clearer error messages
    Signed-off-by: eecs4413 friends <sunglassbungalow@gmail.com>

--- app.py
+++ app.py
@@ -50,22 +50,61 @@ def get_store(store_id):
 
 
 @app.route("/add_store", methods=["POST"])
-def add_store() -> json:
+def add_store():
     """
     Handle requests to the 127.0.0.1:5000/add_store page to add a store.
+    Checks whether the store exists
 
     Returns:
         Response object: JSON response for single store added if successful; else Error Message.
     """
     store_data_received = request.get_json()
+
     store_id = uuid.uuid4().hex
     store = {**store_data_received, "Store_ID": store_id}
     stores[store_id] = store
-    return jsonify(store), {"Success": " Store Added"}
+    return jsonify(store, {"Success": " Store Added"}, 200)
+
+
+@app.delete("/stores/<string:store_id>")
+def delete_store(store_id):
+    """
+    Handle requests to the /stores/store_id delete page.
+
+    Returns:
+        Response object: JSON response for single item.
+    """
+
+    try:
+        store = stores[store_id]
+        del stores[store_id]
+        return jsonify(store, {"Message": "Successfully deleted"})
+
+    except KeyError:
+        return jsonify({"Error": "Store not found"})
+
+
+@app.put("/stores/<string:store_id>")
+def update_store(store_id):
+    """
+    Handle requests to the /stores/store_id update page.
+
+    Returns:
+        Response object: JSON response for single item.
+    """
+
+    try:
+        store_data_received = request.get_json()
+        store = stores[store_id]
+        store |= store_data_received
+        return jsonify(store, {"Message": "Successfully updated"})
+
+    except KeyError:
+        return jsonify({"Error": "Store not found"})
 
 
 @app.route("/add_item", methods=["POST"])
-def add_item() -> json:
+def add_item():
     """
     Handle requests to the 127.0.0.1:5000/add_item page to add an item.
     We expect the add_item request
@@ -75,12 +114,10 @@ def add_item() -> json:
         Response object: JSON response for single item added if successful; else Error Message.
     """
     item_data_received = request.get_json()
-    if item_data_received["Store_ID"] not in stores:
-        return {"Error!": "Store not found"}, 404
     item_id = uuid.uuid4().hex
     item = {**item_data_received, "Item_ID": item_id}
     items[item_id] = item
-    return jsonify(item), {"Success": " Item Added"}
+    return jsonify(item, {"Success": " Item Added"}, 200)
 
 
 @app.route("/items", methods=["GET"])
@@ -106,7 +143,44 @@ def get_item(item_id):
     try:
         return items[item_id]
     except KeyError:
-        return jsonify({"Error": "Item not found"}), 404
+        return jsonify({"Error": "Item not found"}, 404)
+
+
+@app.delete("/items/<string:item_id>")
+def delete_item(item_id):
+    """
+    Handle requests to the /items/item_id delete page.
+
+    Returns:
+        Response object: JSON response for single item.
+    """
+
+    try:
+        item = items[item_id]
+        del items[item_id]
+        return jsonify(item, {"Message": "Successfully deleted"}, 200)
+
+    except KeyError:
+        return jsonify({"Error": "Item not found"}, 404)
+
+
+@app.put("/items/<string:item_id>")
+def update_item(item_id):
+    """
+    Handle requests to the /items/item_id update page.
+
+    Returns:
+        Response object: JSON response for single item.
+    """
+
+    try:
+        item_data_received = request.get_json()
+        item = items[item_id]
+        item |= item_data_received
+        return jsonify(item, {"Message": "Successfully updated"}, 200)
+
+    except KeyError:
+        return jsonify({"Error": "Item not found"}, 404)
 
 
 if __name__ == "__main__":
--- db.py
+++ db.py
@@ -2,10 +2,12 @@
 This module designs and implement a simple static database to be used by Flask application.
 """
 
-stores = {}  # "Halifax", "Toronto", "London" to be added with auto generated uuid
+stores = {}
+
+# "Halifax", "Toronto", "London" to be added with auto generated uuid
+
 
 items = {}
-
 # {"book_name": "Harry Potter", "Lease period in days": 20},
 #
 # {"book_name": "Python 3.11", "Lease period in days": 15},
